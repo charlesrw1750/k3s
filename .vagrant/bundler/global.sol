2f299be1f9f06443b0c0a84a55f0740e9f87b17d
Charles Fabri <144963126+charlesrw1750@users.noreply.github.com>
2024-03-28 23:17:15 -0300

    Criação completa do k3s com Kubectl configurado

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"b9925bf1c1da34f42f3dc894be7b55f2b55f217a8809540771f672b9a7f1d9e2","vagrant_version":"2.4.1"}
